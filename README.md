52478595bb09da9d601dceef59d3da839803eef0
charisTheo <dev@charistheo.io>
2021-12-13 21:06:33 +0200

    Update README.md

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # sendbird-calls-javascript
+
+> This sample is used only for calling another user - does not accept any incoming calls
+
+## Install
+
+```sh
+npm i
+```
+
+## Run
+
+```sh
+npm start
+```
+
+## How to use
+
+Once you have ran this sample app with `npm start`, open your web browser at [127.0.0.1:8080](http://127.0.0.1:8080/).
+
+Then login into Sendbird Calls using your App ID and a user ID.
+
+Once login was succefull, simply type the User ID you would like to call in the text input box and click the 'Call' button.
+> You will have to have Sendbird Calls connected with the callee user ID in order to test receiving a call. As this demo cannot be used for receiving calls, it is suggested to use one of the following for the callee user: [Android app](https://play.google.com/store/apps/details?id=com.sendbird.calls.quickstart), [iOS app](https://apps.apple.com/us/app/sendbird-calls/id1503477603) or Sendbird Dashboard Calls Studio located at `https://dashboard.sendbird.com/<YOUR APP ID>/calls/studio/direct`.
+
+The will try calling 3 times before hanging up for good - **_only if the calee didn't answer_**
+
+You can change the ringing timeout through the `RINGING_TIMEOUT_SECONDS` variable inside `index.js`.
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var APP_ID, USER_ID, call, retries = 0, RINGING_TIMEOUT_SECONDS = 30;
+
 const loginWrapper = document.querySelector('#login-wrapper');
 const appIdInput = document.querySelector('#app_id');
 const userIdInput = document.querySelector('#user_id');
@@ -8,7 +10,6 @@ const userToCallInput = document.querySelector('#user-to-call');
 const callView = document.querySelector('#call-view');
 const endCallButton = document.querySelector('#end-call');
 
-var APP_ID, USER_ID, call, hasAnswered = false, retries = 0;
 appIdInput.addEventListener('input', checkValidity)
 userIdInput.addEventListener('input', checkValidity)
 loginButton.addEventListener('click', login)
@@ -29,11 +30,11 @@ function login() {
   USER_ID = userIdInput.value;
 
   SendBirdCall.init(APP_ID)
-  SendBirdCall.setRingingTimeout(3)
+  SendBirdCall.setRingingTimeout(RINGING_TIMEOUT_SECONDS)
   SendBirdCall.authenticate({ userId: USER_ID }, (result, error) => {
     if (error) {
       loginButton.disabled = false;
-      console.error(error);
+      alert(`Failed to authenticate. Error: ${error}`)
       return
     }
     appIdInput.removeEventListener('input', checkValidity);
@@ -41,15 +42,15 @@ function login() {
     loginButton.removeEventListener('input', login);
     loginWrapper.style.display = 'none';
 
-    // Establishing websocket connection.
+    // Establishing websocket connection after authenticating
     SendBirdCall.connectWebSocket()
-        .then(() => {
-          callOptions.style.display = 'block'
-        })
-        .catch(/* Failed to connect */);
+      .then(() => {
+        callOptions.style.display = 'block'
+      })
+      .catch((error) => {
+        alert(`Failed to connect websocket. Error: ${error}`)
+      });
   });
-
-
 }
 
 function callUser() {
@@ -65,36 +66,34 @@ function callUser() {
   };
 
   call = SendBirdCall.dial(dialParams, (call, error) => {
-      if (error) {
-          console.error(error)
-      }
-      callView.style.display = 'flex'
-      callView.querySelector('#call-user-id').innerText = `Calling ${userToCall}...`
+    if (error) {
+      alert(`Failed to call. Error: ${error}`)
+    }
+    callView.style.display = 'flex'
+    callView.querySelector('#call-user-id').innerHTML = `<div>Calling ${userToCall}...</div><div>Times tried calling: ${retries}</div>`
   });
-
-  call.onConnected = (call) => {
-    hasAnswered = true
-  };
 
   call.onEnded = (call) => {
     const endResult = call._endResult
     call = null
-    callView.style.display = 'none'
 
-    if (hasAnswered || retries >= 3 || endResult !== "no_answer") {
+    if (retries >= 3 || endResult !== "no_answer") {
+      callView.style.display = 'none'
       retries = 0
-      hasAnswered = false
       return
     }
+
+    // Retry if no answer
     setTimeout(() => {
       callUser()
     }, 1000)
   };
 }
 
+// User has manually ended the call
 function endCall() {
   call.end()
   call = null
-  hasAnswered = false;
+  retries = 0
   callView.style.display = 'none'
 }
